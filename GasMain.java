c06f06e8fc649aeb8d6fa5dfcb9b45e5dd74af5a
TsumiyaZ <watcharakornnigc@gmail.com>
2025-07-27 19:48:08 +0700

    Up

--- GasMain.java
+++ GasMain.java
@@ -2,5 +2,6 @@ public class GasMain {
     public static void main(String[] args) {
         GasOPP start = new GasOPP();
         start.setVisible(true);
+        System.out.println("Hello world");
     }
 }
